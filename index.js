781d6543e3c40429f29dec401f6245482158b089
Gavin-Persad <gavinapersad@gmail.com>
2024-10-15 15:38:42 +0100

    tasks complete

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send({
+		message: 'Hello World!',
+		message2: 'Hello World2!',
+	});
 });
 
-// Start the server
-const PORT = 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });

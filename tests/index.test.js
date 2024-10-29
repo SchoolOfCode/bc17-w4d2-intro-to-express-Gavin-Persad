641207dea1d90f1ab5b95684b9d65a1473af449b
Gavin-Persad <gavinapersad@gmail.com>
2024-10-29 10:39:13 +0000

    tests now failing without index.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,17 +1,9 @@
 import request from 'supertest';
-import express from 'express';
+import app from '../index.js'; // Import the app from index.js
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-	res.send({
-		message: 'Hello World!',
-		message2: 'Hello World2!',
-	});
-});
 
 describe('Express Server', () => {
 	let server;
@@ -29,11 +21,6 @@ describe('Express Server', () => {
 		expect(response.status).toBe(200);
 		expect(response.headers['content-type']).toMatch(/json/);
 		expect(response.body).toBeInstanceOf(Object);
-	});
-
-	it('should listen on the correct port', () => {
-		const port = server.address().port;
-		expect(port).toBe(Number(process.env.PORT) || 3000);
 	});
 
 	it('should return a JSON object with at least one key-value pair', async () => {
